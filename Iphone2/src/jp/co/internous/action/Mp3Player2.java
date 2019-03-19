9bf4c6420f27c8295dbc8cb12160d9b6613391b3
t-akane <peach.apple.mango0147@gmail.com>
2019-03-19 14:06:03 +0900

    0319

---
+++ Iphone2/src/jp/co/internous/action/Mp3Player2.java
@@ -0,0 +1,10 @@
+package jp.co.internous.action;
+
+public interface Mp3Player2 {
+
+	public abstract void play();
+	public abstract void stop();
+	public abstract void next();
+	public abstract void back();
+
+}
---
+++ Iphone2/src/jp/co/internous/action/Phone.java
@@ -0,0 +1,29 @@
+package jp.co.internous.action;
+
+public class Phone {
+
+	public void play(){
+		System.out.println("再生");}
+
+	public void stop(){
+		System.out.println("停止");}
+
+	public void next(){
+		System.out.println("再生");}
+
+	public void back(){
+		System.out.println("再生");}
+
+	public void call(){
+		System.out.println("再生");}
+
+	public void mail(){
+		System.out.println("再生");}
+
+	public void photo(){
+		System.out.println("再生");}
+
+	public void internet(){
+		System.out.println("再生");}
+
+}
